7708f6300d57d7114fe3255a3f0931387f151b21
Nick Mirasol <takutastrong@yahoo.com>
2013-04-23 21:13:52 -0500

    Added armor folder

---
+++ blfngl/fallout/armor/ArmorBaseEnclave.java
@@ -0,0 +1,35 @@
+package blfngl.fallout.armor;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.IArmorTextureProvider;
+import blfngl.fallout.common.FalloutMain;
+
+public class ArmorBaseEnclave extends ItemArmor implements IArmorTextureProvider
+{
+
+	public ArmorBaseEnclave(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4)
+	{
+		super(par1, par2EnumArmorMaterial, par3, par4);
+	}
+
+	public void func_94581_a(IconRegister iconRegister)
+	{
+		iconIndex = iconRegister.registerIcon("blfngl" + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+	}
+
+	public String getArmorTextureFile(ItemStack par1)
+	{
+		if ( par1.itemID==FalloutMain.EnclaveHelm.itemID|| par1.itemID==FalloutMain.EnclaveChest.itemID|| par1.itemID==FalloutMain.EnclaveBoots.itemID)
+		{
+			return "/blfngl/fallout/textures/ADV1_1.png";
+		}
+		if(par1.itemID==FalloutMain.EnclaveLegs.itemID)
+		{
+			return "/blfngl/fallout/textures/ADV1_2.png";
+		}
+		return "/blfngl/fallout/textures/ADV1_1.png";
+	}      
+}
---
+++ blfngl/fallout/armor/ArmorBaseHellfire.java
@@ -0,0 +1,35 @@
+package blfngl.fallout.armor;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.IArmorTextureProvider;
+import blfngl.fallout.common.FalloutMain;
+
+public class ArmorBaseHellfire extends ItemArmor implements IArmorTextureProvider
+{
+
+	public ArmorBaseHellfire(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4)
+	{
+		super(par1, par2EnumArmorMaterial, par3, par4);
+	}
+
+	public void func_94581_a(IconRegister iconRegister)
+	{
+		iconIndex = iconRegister.registerIcon("blfngl" + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+	}
+
+	public String getArmorTextureFile(ItemStack par1)
+	{
+		if ( par1.itemID==FalloutMain.HellHelm.itemID|| par1.itemID==FalloutMain.HellChest.itemID|| par1.itemID==FalloutMain.HellBoots.itemID)
+		{
+			return "/blfngl/fallout/textures/ADV2_1.png";
+		}
+		if(par1.itemID==FalloutMain.HellLegs.itemID)
+		{
+			return "/blfngl/fallout/textures/ADV2_2.png";
+		}
+		return "/blfngl/fallout/textures/ADV2_1.png";
+	}      
+}
